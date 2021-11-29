e9b765926a9210c4b050cb2a4e576101854aee1e
Ryan Oberholzer <ryanoberholzer@gmail.com>
2021-11-29 14:25:48 +0200

    Remove key

--- lib/sayagain.rb
+++ lib/sayagain.rb
@@ -2,11 +2,12 @@ require "sayagain/version"
 require "nokogiri"
 require "dry/cli"
 require "easy_translate"
+require 'dotenv/load'
 
 require_relative "./sayagain/cli"
 require_relative "./sayagain/cli/commands"
 
-EasyTranslate.api_key = "AIzaSyBak3EpPaqmi9A3-qCF7pQ03Cfgg-RaKrk"
+EasyTranslate.api_key = ENV['GOOGLE_TRANSLATE_KEY']
 
 module SayAgain
   class Error < StandardError; end
